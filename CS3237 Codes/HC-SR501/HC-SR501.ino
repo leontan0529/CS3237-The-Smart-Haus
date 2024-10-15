4696a83d3e5ed3ff3a028c5bd724ce90661fbc83
DESKTOP-Q41L7JF\Manoj <theerazor@outlook.com>
2024-10-15 11:56:27 +0800

    Update code to print continously to serial

--- CS3237 Codes/HC-SR501/HC-SR501.ino
+++ CS3237 Codes/HC-SR501/HC-SR501.ino
@@ -1,46 +1,27 @@
-/*
- * This ESP32 code is created by esp32io.com
- *
- * This ESP32 code is released in the public domain
- *
- * For more detail (instruction and wiring diagram), visit https://esp32io.com/tutorials/esp32-motion-sensor
- */
-
-const int PIN_TO_SENSOR = 19; // GPIO19 pin connected to OUTPUT pin of sensor
-int pinStateCurrent   = LOW;  // current state of pin
-int pinStatePrevious  = LOW;  // previous state of pin
-const unsigned long DELAY_TIME_MS = 30000; // 30000 miliseconds ~ 30 seconds
-bool delayEnabled = false;
-unsigned long delayStartTime;
-
+// Define the pin for the HC-SR501 motion sensor
+#define PIR_SENSOR_PIN 13 // Adjust according to the GPIO pin you're using
 
 void setup() {
-  Serial.begin(9600);            // initialize serial
-  pinMode(PIN_TO_SENSOR, INPUT); // set ESP32 pin to input mode to read value from OUTPUT pin of sensor
+  // Initialize the serial communication
+  Serial.begin(115200);
+  
+  // Set PIR sensor pin as input
+  pinMode(PIR_SENSOR_PIN, INPUT);
+  
+  Serial.println("PIR Motion Sensor Test - Continuous Updates");
 }
 
 void loop() {
-  pinStatePrevious = pinStateCurrent; // store old state
-  pinStateCurrent = digitalRead(PIN_TO_SENSOR);   // read new state
-
-  if (pinStatePrevious == LOW && pinStateCurrent == HIGH) {   // pin state change: LOW -> HIGH
+  // Read PIR sensor value
+  int sensorValue = digitalRead(PIR_SENSOR_PIN);
+  
+  // Print continuous updates based on the sensor state
+  if (sensorValue == HIGH) {
     Serial.println("Motion detected!");
-    Serial.println("Turning on / activating");
-    delayEnabled = false; // disable delay
-    // TODO: turn on alarm, light or activate a device ... here
-  }
-  else
-  if (pinStatePrevious == HIGH && pinStateCurrent == LOW) {   // pin state change: HIGH -> LOW
-    Serial.println("Motion stopped!");
-    Serial.println("Motion stopped!");
-    delayEnabled = true; // enable delay
-    delayStartTime = millis(); // set start time
-    // TODO: turn off alarm, light or deactivate a device ... here
+  } else {
+    Serial.println("No motion detected.");
   }
 
-  if (delayEnabled == true && (millis() - delayStartTime) >= DELAY_TIME_MS) {
-    Serial.println("Turning off / deactivating");
-    delayEnabled = false; // disable delay
-    // TODO: turn off alarm, light or deactivate a device ... here
-  }
+  // Delay for readability in the serial monitor
+  delay(1000); // Adjust delay time as needed (1000ms = 1 second)
 }
