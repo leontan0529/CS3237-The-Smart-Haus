f8b3dcf365f96047181480be4d5ac0bc00cf4e34
DESKTOP-Q41L7JF\Manoj <theerazor@outlook.com>
2024-11-21 10:11:22 +0800

    Modify IP

--- CS3237 Codes/Combined Codes/ESP32_cam/ESP32_cam.ino
+++ CS3237 Codes/Combined Codes/ESP32_cam/ESP32_cam.ino
@@ -8,24 +8,31 @@
 
 #define LED_FLASH_PIN 4  // GPIO pin for the LED (on some ESP32-CAMs, it's GPIO 4)
 
-const char* ssid = "ESP32_Access_Point";
-const char* password = "password123";
+const char* ssid = "MitsubishiAttrage";
+const char* password = "Toyota Vios";
 
-String serverName = "192.168.4.2";
+String serverName = "192.168.115.8";
 
 String serverPath = "/upload";
 
-const int serverPort = 5000;
+const int serverPort = 9091;
 
 bool camera_status = 0;
 bool photo_taken = 0;
 
 WiFiClient client;
 
+/*
 // Set your Static IP address
 IPAddress local_IP(192, 168, 4, 30);
 // Set your Gateway IP address
 IPAddress gateway(192, 168, 4, 1);
+IPAddress subnet(255, 255, 255, 0);
+*/
+
+// Set your Static IP address
+IPAddress local_IP(192, 168, 115, 40);
+IPAddress gateway(192, 168,115, 159);
 IPAddress subnet(255, 255, 255, 0);
 
 // Web server running on port 80
@@ -53,45 +60,7 @@ WebServer server(80);
 const int timerInterval = 30000;    // time between each HTTP POST image
 unsigned long previousMillis = 0;   // last time image was sent
 
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <style>
-    body { text-align:center; }
-    .vert { margin-bottom: 10%; }
-    .hori{ margin-bottom: 0%; }
-  </style>
-</head>
-<body>
-  <div id="container">
-    <h2>ESP32-CAM Last Photo</h2>
-    <p>It might take more than 5 seconds to capture a photo.</p>
-    <p>
-      <button onclick="rotatePhoto();">ROTATE</button>
-      <button onclick="capturePhoto()">CAPTURE PHOTO</button>
-      <button onclick="location.reload();">REFRESH PAGE</button>
-    </p>
-  </div>
-  <div><img src="saved-photo" id="photo" width="70%"></div>
-</body>
-<script>
-  var deg = 0;
-  function capturePhoto() {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', "/capture", true);
-    xhr.send();
-  }
-  function rotatePhoto() {
-    var img = document.getElementById("photo");
-    deg += 90;
-    if(isOdd(deg/90)){ document.getElementById("container").className = "vert"; }
-    else{ document.getElementById("container").className = "hori"; }
-    img.style.transform = "rotate(" + deg + "deg)";
-  }
-  function isOdd(n) { return Math.abs(n % 2) == 1; }
-</script>
-</html>)rawliteral";
+
 
 void handlePost() {
   if (server.hasArg("plain") == false) {
